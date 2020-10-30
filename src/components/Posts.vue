a520325c23137870439cdd3ac22e17816220387e
JamesAlexander-rockwall <james.alexander.rockwall@gmail.com>
2020-10-30 20:48:31 +0200

    Initial commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="posts">
+      <h1>Posts</h1>
+      This file will list all the posts
+
+      <div v-for='post in posts' :key="post.title">
+        <p>
+          <span><b>{{ post.title }}</b></span><br/>
+          <span>{{ post.description }}</span>
+        </p>
+      </div>
+    </div>
+</template>
+
+<script>
+import PostsService from '../services/PostsService'
+
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
